d45f68a5858799e8782953bc8d3d5bec5f4694ce
emysdias <diassousa@yahoo.com.br>
2025-10-31 20:15:01 -0300

    feat: add constants

---
+++ src/constants/numbers.js
@@ -0,0 +1,15 @@
+export const DEFAULT_PX_PER_DAY = 30;
+export const PX_PER_DAY_MIN = 20;
+export const PX_PER_DAY_MAX = 80;
+export const ZOOM_STEP = 4;
+
+export const LANE_HEIGHT = 44;
+export const LANE_GAP = 8;
+export const MIN_DURATION_DAYS = 1;
+export const EXTRA_CONTENT_WIDTH = 200;
+
+export const MS_PER_DAY = 24 * 60 * 60 * 1000;
+export const INITIAL_LANES_COUNT = 0;
+
+export const MONTH_INDEX_MAX = 11;
+export const PAD_MONTH_WIDTH = 2;
